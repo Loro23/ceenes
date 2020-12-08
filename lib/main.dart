a0aca510da03c9695339ed31dc6859315a4d83d5
Benjamin Kasten <bkasten@mail.uni-paderborn.de>
2020-12-08 22:44:03 +0100

    initial api requests

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'api.dart';
+import 'movie.dart';
 import 'package:tmdb_api/tmdb_api.dart';
 
 void main() {
@@ -12,18 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
+
       home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
@@ -31,15 +21,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -50,27 +31,48 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
+  List movies = [];
  _getTrendingMovies()async{
-    Map result = await tmdb.v3.movies.getPouplar(language: 'de');
+    Map result = await tmdb.v3.discover.getMovies(page: 1, language: 'de',withOrginalLanguage: 'de', );
     List resulties = result.values.toList();
-    print(resulties[1].length);
     for (int i = 0; i <20; i++){
-      print(resulties[1][i]['id']);
-    };
+      int id = resulties[1][i]['id'];
+      String title = resulties[1][i]['title'];
+      String original_title = resulties[1][i]['original_title'];
+      String overview = resulties[1][i]['overview'];
+      List genre_ids = resulties[1][i]['genre_ids'];
+      String poster_path = resulties[1][i]['poster_path'];
+      double popularity = resulties[1][i]['popularity'];
+      double vote_average = resulties[1][i]['vote_average'];
+      double vote_count = resulties[1][i]['vote_count'];
+
+      Movie movie = Movie(id, title, original_title, overview, genre_ids,
+      poster_path, popularity, vote_average, vote_count );
+      movies.add(movie);
+      print(movie.title);
+
+      Map result = await tmdb.v3.movies.getDetails(movie.id, appendToResponse: "watch/providers");
+      print(result);
+      print('');
+
+
+
+    }
+
+
   }
 
   @override
   Widget build(BuildContext context) {
+   _getTrendingMovies();
 
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
+
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+
         child: Column(
           // Column is also a layout widget. It takes a list of children and
           // arranges them vertically. By default, it sizes itself to fit its
@@ -99,7 +101,7 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _getTrendingMovies,
+        onPressed: (){},
         tooltip: 'Increment',
         child: Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
---
+++ lib/movie.dart
@@ -0,0 +1,28 @@
+import 'dart:core';
+
+class Movie {
+  var id;
+  var title;
+  var original_title;
+  var rating;
+  var overview;
+  var genre_ids;
+  var popularity;
+  var poster_path;
+  var vote_average;
+  var vote_count;
+
+  Movie(int id, String title, String original_title, String overview,
+      List genre_ids,  String poster_path,double popularity,
+      double vote_average, double vote_count) {
+    this.id = id;
+    this.title = title;
+    this.original_title = original_title;
+    this.overview = overview;
+    this.genre_ids = genre_ids;
+    this.poster_path = poster_path;
+    this.popularity = popularity;
+    this.vote_average = vote_average;
+    this.vote_count = vote_count;
+  }
+}
