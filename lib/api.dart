c712cb2b9a4b3751bedc052423b60ff964b4a641
Lorenz <enzolop6@gmail.com>
2020-12-08 21:03:45 +0100

    initial commit connection to api made

---
+++ lib/api.dart
@@ -0,0 +1,5 @@
+import 'package:tmdb_api/tmdb_api.dart';
+
+TMDB tmdb = TMDB( //TMDB instance
+    ApiKeys('350adaf4f8637366851fa911be0b1696', 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzNTBhZGFmNGY4NjM3MzY2ODUxZmE5MTFiZTBiMTY5NiIsInN1YiI6IjVmY2ZjNjcyNzMxNGExMDA0MWQwODY3OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.MOvfFdI_BA1C56_fuEQfy14VuHGbYYscIMoPXt2p8Hg'),//ApiKeys instance with your keys,
+  );
